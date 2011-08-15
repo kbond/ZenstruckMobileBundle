fb6dc3b68b02fee14d6e0bd6dd182aa2ab0b8a59
kbond <kevinbond@gmail.com>
2011-08-15 17:38:10 -0400

    started feature to have mobile templates

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -12,7 +12,13 @@ class WelcomeController extends Controller
         $mobile = $this->container->get('zenstruck_mobile.manager')->getMobile();
 
         $response = new Response();
-        $response->setContent($this->renderView('AcmeDemoBundle:Welcome:index.html.twig', array('mobile' => $mobile)));
+
+        if ($this->get('templating')->exists('AcmeDemoBundle:Welcome:mobile\index.html.twig') && $mobile) {
+            $response->setContent($this->renderView('AcmeDemoBundle:Welcome:mobile\index.html.twig', array('mobile' => $mobile)));
+        } else {
+            $response->setContent($this->renderView('AcmeDemoBundle:Welcome:index.html.twig', array('mobile' => $mobile)));
+        }
+
         $response->setSharedMaxAge(30);
 
         return $response;
