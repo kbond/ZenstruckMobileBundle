7f0f2ff2922b91a7cfeb255caeecc01a3fcc0bb1
Kevin Bond <kevinbond@gmail.com>
2013-02-18 09:02:42 -0500

    fixed twig engine

--- Twig/TwigEngine.php
+++ Twig/TwigEngine.php
@@ -5,6 +5,7 @@ namespace Zenstruck\Bundle\MobileBundle\Twig;
 use Symfony\Bundle\TwigBundle\TwigEngine as BaseTwigEngine;
 use Symfony\Bundle\FrameworkBundle\Templating\GlobalVariables;
 use Symfony\Component\Templating\TemplateNameParserInterface;
+use Symfony\Component\Config\FileLocatorInterface;
 use Zenstruck\Bundle\MobileBundle\Manager\MobileManager;
 
 /**
@@ -14,19 +15,18 @@ class TwigEngine extends BaseTwigEngine
 {
     protected $mobileManager;
 
-    public function __construct(\Twig_Environment $environment, TemplateNameParserInterface $parser,
-            MobileManager $mobileManager, GlobalVariables $globals = null)
+    public function __construct(\Twig_Environment $environment, TemplateNameParserInterface $parser, FileLocatorInterface $locator, MobileManager $mobileManager)
     {
-        parent::__construct($environment, $parser, $globals);
-
         $this->mobileManager = $mobileManager;
+
+        parent::__construct($environment, $parser, $locator);
     }
 
     public function render($name, array $parameters = array())
     {
         if ($this->mobileManager->isMobile()) {
             $mobileTemplate = preg_replace("/^\w+:\w+:/", '$0mobile/', $name);
-            
+
             if ($this->exists($mobileTemplate)) {
                 $name = $mobileTemplate;
             }
